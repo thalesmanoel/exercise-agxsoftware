a6e7cd2c3ace2ffde28d6858329550ad05806c50
Thales Manoel <thalesdesenvolvedor@gmail.com>
2025-07-10 16:37:03 -0300

    feat: initial project setup

---
+++ src/config/mongo.ts
@@ -0,0 +1,9 @@
+/// <reference lib="deno.ns" />
+import mongoose from 'npm:mongoose@7';
+import "https://deno.land/std@0.224.0/dotenv/load.ts";
+
+const MONGO_URI = Deno.env.get("MONGO_URI") || "";
+
+await mongoose.connect(MONGO_URI);
+
+export { mongoose };
