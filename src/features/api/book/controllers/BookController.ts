0bdee7661cbb7120938a9ea0f6f78501f5863b21
thalesmanoel <thalesdesenvolvedor@gmail.com>
2025-07-11 10:38:04 -0300

    feat: create controller for Book model

---
+++ src/features/api/book/controllers/BookController.ts
@@ -0,0 +1,86 @@
+import { Request, Response, NextFunction } from 'npm:express';
+import { Types } from 'npm:mongoose';
+import { throwlhos } from '../../../../globals/Throwlhos.ts';
+import { BookService } from './services/BookService.ts';
+
+class BookController {
+  private bookService: BookService;
+
+  constructor({ bookService = new BookService() } = {}) {
+    this.bookService = bookService;
+  }
+
+  index = async (res: Response, next: NextFunction) => {
+    try {
+      const books = await this.bookService.findAll();
+      return res.send_ok('Livros encontrados.', books);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  show = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      if (!Types.ObjectId.isValid(id)) {
+        throw throwlhos.err_badRequest('ID inválido.', { id });
+      }
+
+      const book = await this.bookService.findById(id);
+      if (!book) {
+        throw throwlhos.err_notFound('Livro não encontrado.', { id });
+      }
+
+      return res.send_ok('Livro encontrado.', book);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const book = await this.bookService.create(req.body);
+      return res.send_ok('Livro criado com sucesso.', { _id: book._id });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  update = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      if (!Types.ObjectId.isValid(id)) {
+        throw throwlhos.err_badRequest('ID inválido.', { id });
+      }
+
+      const updated = await this.bookService.update(id, req.body);
+      if (!updated) {
+        throw throwlhos.err_notFound('Livro não encontrado para atualização.', { id });
+      }
+
+      return res.send_ok('Livro atualizado com sucesso.', updated);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  destroy = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      if (!Types.ObjectId.isValid(id)) {
+        throw throwlhos.err_badRequest('ID inválido.', { id });
+      }
+
+      const deleted = await this.bookService.delete(id);
+      if (!deleted) {
+        throw throwlhos.err_notFound('Livro não encontrado para exclusão.', { id });
+      }
+
+      return res.send_ok('Livro removido com sucesso.', deleted);
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
+export { BookController };
---
+++ src/globals/Throwlhos.ts
@@ -0,0 +1,3 @@
+import npmTthrowlhos from 'npm:throwlhos'
+
+export const throwlhos = npmTthrowlhos.default
