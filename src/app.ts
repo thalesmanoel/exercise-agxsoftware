7e939c2fcc0df843db32f52d64d3bf1266b0e21b
thalesmanoel <thalesdesenvolvedor@gmail.com>
2025-07-11 15:27:10 -0300

    feat: add package morgan to project

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from "npm:express@4.18.2";
 import "./config/mongo.ts"; 
 import { BookRouter } from "./features/api/book/BookRouter.ts";
 import responser from 'npm:responser@1'
+import morgan from 'npm:morgan@1.10.0';
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(responser.default);
 app.use(BookRouter);
